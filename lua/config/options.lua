b73c2a1bd0042c214d90eef3bdfe465cec7b5a4f
Edy Espinal <edyespinal@gmail.com>
2025-11-10 08:13:41 +0100

    Let there be music

---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.have_nerd_font = true
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,16 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 
+    'nvim-tree/nvim-web-devicons',
+    opt = true
+  },
+  config = function()
+    require("lualine").setup({
+      options = {
+        icons_enabled = true,
+        theme = "palenight",
+        globalstatus = true
+      }
+    })
+  end
+}
