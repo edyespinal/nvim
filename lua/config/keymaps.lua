fdaa05af28d02e10d68836bfbfb3dd8133c3e9e1
Edy Espinal <edyespinal@gmail.com>
2025-11-20 18:58:02 +0100

    Add mini move plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,6 @@
--- Move lines up and down
-vim.keymap.set("n", "§", "ddkP")
-vim.keymap.set("n", "¶", "ddp")
+-- File management
+vim.keymap.set({ "n", "v" }, "<D-s>", ":w<CR>", { silent = true, desc = "Save file" })
 
 -- Navigate between buffers
-vim.keymap.set("n", "<Right>", ":bnext<CR>")
-vim.keymap.set("n", "<Left>", ":bprevious<CR>")
+vim.keymap.set("n", "<leader><Right>", ":bnext<CR>", { silent = true, desc = "Next buffer" })
+vim.keymap.set("n", "<leader><Left>", ":bprevious<CR>", { silent = true, desc = "Previous buffer" })
---
+++ lua/plugins/mini-move.lua
@@ -0,0 +1,21 @@
+return {
+	"nvim-mini/mini.move",
+	version = "*",
+	config = function()
+		require("mini.move").setup({
+			mappings = {
+				left = "<M-h>",
+				right = "<M-l>",
+				down = "<M-j>",
+				up = "<M-k>",
+				line_left = "<M-h>",
+				line_right = "<M-l>",
+				line_down = "<M-j>",
+				line_up = "<M-k>",
+			},
+			options = {
+				reindent_linewise = true,
+			},
+		})
+	end,
+}
